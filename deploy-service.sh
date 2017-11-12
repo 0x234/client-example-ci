c54057aba2167eb6c7f4cbe2cc3a9b5177527c3a
Jake Bunce <jakebunce@gmail.com>
2017-11-12 23:38:24 +0000

    [client] Echo progress of the deployment

--- deploy-service.sh
+++ deploy-service.sh
@@ -2,21 +2,34 @@
 
 set -e
 
+echo "Building docker image"
 docker build -f client.dockerfile -t eu.gcr.io/true-energy-185810/client:$TRAVIS_COMMIT .
+
+echo "Decoding base64 for gcloud authentication"
 
 echo $GCLOUD_SERVICE_KEY_STG | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+
+echo "Setting gcloud config"
 
 gcloud --quiet config set project tokencard
 gcloud --quiet config set container/cluster tokencard
 gcloud --quiet config set compute/zone europe-west1-b
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME_STG
 
-gcloud docker push eu.gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}
+echo "Push docker image to the gcloud registry"
 
-yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}:latest
+gcloud docker push eu.gcr.io/true-energy-185810/client
+
+echo "Add latest tag to this build"
+
+yes | gcloud beta container images add-tag eu.gcr.io/true-energy-185810/client:$TRAVIS_COMMIT eu.gcr.io/true-energy-185810/client::latest
+
+echo "View cluster info"
 
 kubectl config view
 kubectl config current-context
+
+echo "Set image to be the new build"
 
 kubectl set image deployment/client-deployment client=eu.gcr.io/true-energy-185810/client:$TRAVIS_COMMIT
