885436ae01252a550b3f778a4cf186c4626900c0
Jake Bunce <jakebunce@gmail.com>
2017-11-12 22:50:05 +0000

    [client] Modify Go import path for local libraries

--- client/client.go
+++ client/client.go
@@ -32,8 +32,8 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
-	pb "./routeguide"
-	"./testdata"
+	pb "src/routeguide"
+	"src/testdata"
 )
 
 var (
