0a4023da50bfcca2e228de63ddfeb36b1082bdf6
Jake Bunce <jakebunce@gmail.com>
2017-11-12 22:58:02 +0000

    [client] Add full Github URL path for import

--- client/client.go
+++ client/client.go
@@ -32,8 +32,8 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
-	pb "./src/routeguide"
-	"./src/testdata"
+	pb "github.com/jbunce/client-example-ci/routeguide"
+	"github.com/jbunce/client-example-ci/testdata"
 )
 
 var (
